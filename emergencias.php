cfc4c11b2ba9b5cd9ca96a4a1a6b57748a29f9e3
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2015-11-19 00:23:03 -0500

    scrappy emergencias

---
+++ emergencias.php
@@ -0,0 +1,35 @@
+<?php
+ include('simple_html_dom.php');
+
+ $html = file_get_html('http://www.bomberosperu.gob.pe/po_diario.asp');
+
+ $contador = 0;
+ $cron = array();
+ $emergencia = array();
+
+ foreach($html->find('tr') as $div) {
+ 	$contador++;
+ 	if($contador>10):
+
+        $emergencia['tipo'] = str_replace("&nbsp;","",$div->find('td.lineaizq', 4)->plaintext);
+        $emergencia['direccion'] = str_replace("&nbsp;","",$div->find('td.lineaizq', 3)->plaintext);
+	 	$mapa = $div->find('img', 0);
+	 	$valores_map = str_replace("javascript:mapa('","", $mapa->onclick);
+ 		$valor_map = explode("','", $valores_map);
+        $emergencia['lat'] = str_replace("&nbsp;","",$valor_map[0]);
+ 		$emergencia['lng'] = str_replace("&nbsp;","",$valor_map[1]);
+
+        array_push($cron, $emergencia);
+
+ 	endif;
+
+ 	endif;
+ }
+
+print_r(json_encode($cron));
+
+    // clean up memory
+ 	$html->clear();
+ 	unset($html);
+
+ ?>
