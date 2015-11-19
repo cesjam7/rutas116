b845b71422a2f383a1f315ed958e153b4c42ccce
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2015-11-19 01:06:30 -0500

    prueba

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>RUTAS 116</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 	<style>
